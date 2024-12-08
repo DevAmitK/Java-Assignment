cb08e1d43d250971ef697f30ebe50c82f9ff21d2
Amit <amitkundu113920@gmail.com>
2024-12-09 01:18:37 +0530

    Assignment_2 : Question 10

---
+++ src/Assignment_2/Question_10/Student.java
@@ -0,0 +1,11 @@
+package Assignment_2.Question_10;
+
+public class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
