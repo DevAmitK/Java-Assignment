9e4741e4ad2c7425c901f58139d04ef3e935f653
Amit <amitkundu113920@gmail.com>
2024-12-12 23:48:12 +0530

    Assignment_2 : Question 13

---
+++ src/Assignment_3/Question_13/FourWheeler.java
@@ -0,0 +1,11 @@
+package Assignment_3.Question_13;
+
+public interface FourWheeler extends Vehicle{
+    @Override
+    void stop();
+
+    @Override
+    void start();
+
+    void fuelType(String fuel);
+}
---
+++ src/Assignment_3/Question_13/Vehicle.java
@@ -0,0 +1,6 @@
+package Assignment_3.Question_13;
+
+public interface Vehicle {
+    void start();
+    void stop();
+}
