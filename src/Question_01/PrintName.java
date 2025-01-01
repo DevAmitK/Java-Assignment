c48a60a2e0393f0778587efeaa8159fba5f097cb
Amit <amitkundu.developer@gmail.com>
2025-01-01 20:14:37 +0530

    print name , department , class roll  * and collage name

---
+++ src/Question_01/PrintName.java
@@ -0,0 +1,28 @@
+package Question_01;
+
+/**
+ * Write a program to print name , department , class roll
+ * and collage name of a student in new line
+ */
+
+public class PrintName {
+    public static void main(String[] args) {
+
+        /**
+         * It is
+         */
+
+        var name = "Amit Kundu";
+        var dep = "CSE";
+        var roll = 44;
+        var collageName = "MIT";
+
+        System.out.println("\n--- Student Details ---\n" +
+                "Name: " + name + "\n" +
+                "Department: " + dep + "\n" +
+                "Roll Number: " + roll + "\n" +
+                "College Name: " + collageName);
+
+
+    }
+}
